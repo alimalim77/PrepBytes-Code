7042295671ee1bf3ad9694a886a96d584b8d607e
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2023-01-10 09:39:49 +0530

    work

---
+++ ArrayList_ReverseSort.java
@@ -0,0 +1,18 @@
+import java.util.*;    
+public class Main 
+{
+  
+public static void main (String[]args)
+  {
+    ArrayList < String > list = new ArrayList < String > ();
+    list.add ("Apple");
+    list.add ("Orange");
+    list.add ("Cherry");
+    list.add ("Apricot");
+
+
+    System.out.println ("Before: " + list);
+    Collections.sort (list, Collections.reverseOrder());
+    System.out.println ("After: " + list);
+   } 
+} 

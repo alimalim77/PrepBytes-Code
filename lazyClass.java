14668320a82fa44f827af5aded9cb5e2bc490f99
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2023-02-12 15:04:58 +0530

    CodePush

---
+++ lazyClass.java
@@ -0,0 +1,25 @@
+//Java Program for Lazy Instantiation
+
+
+public class lazyClass {
+private static lazyClass instance;
+
+private lazyClass () {}
+
+public static synchronized lazyClass getInstance() {
+
+if(instance == null) {
+instance = new lazyClass ();
+}
+
+return instance;
+}
+
+public void sampleMethod( ) {
+System.out.println("sampleMethod - Singleton Class in java");
+}
+public static void main(String[] args) {
+lazyClass cur=lazyClass.getInstance();
+cur.sampleMethod();
+}
+}
